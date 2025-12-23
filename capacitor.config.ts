77759f64187219104cbdf14325b90e50d8b2134d
1wasilah1 <m.wasilahhadi@gmail.com>
2025-12-24 02:19:16 +0700

    change name app and logo

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
   appId: "com.fuelfriend.app",
-  appName: "Fuel Friend",
+  appName: "FuelFriend Agent",
   webDir: "dist/public",
   bundledWebRuntime: false,
   android: {
